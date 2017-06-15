adb0c40f213fe63805da38512960e02cb1fcf6c4
Tristan Gray <tristangraycc@gmail.com>
2017-06-15 22:20:19 +0100

    Structure there. Failing to claim back data due to access control error

--- react/client/src/components/Home.jsx
+++ react/client/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import PlanetDetail from './PlanetDetail'
 import { Link } from 'react-router'
 
 const Home = () => (
   <div className="home">
     <h1 className='title'>The Solar System</h1>
+    <PlanetDetail/>
   </div>
 )
 
---
+++ react/client/src/components/PlanetDetail.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class PlanetDetail extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      planets: []
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <h3>{this.state.planets.name}</h3>
+        <p>Type of Planet: {this.state.planets.typeofplanet}</p>
+        <p>Size (km): {this.state.planets.size}</p>
+        <p>Distance From Sun (AU): {this.state.planets.distancefromsun}</p>
+        <p>Length of Year (Earth Days): {this.state.planets.distancefromsun}</p>
+        <p>Description: {this.state.planets.desciption}</p>
+        <p>Image: {this.state.planets.image}</p>
+      </div>
+    );
+  }
+
+  componentDidMount(){
+    const url = 'http://localhost:5000/api/planets';
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+
+    request.addEventListener('load', () => {
+      console.log("Making request")
+      if (request.status !== 200) return;
+      const jsonString = request.responseText;
+      const data = JSON.parse(jsonString);
+      console.log(data);
+      this.setState({
+        planets: data[0]
+      });
+    });
+    request.send();
+  }
+}
+
+export default PlanetDetail;
